2d9606fc7642105944ec75376264230fb03881bd
Kazuya Takei <myself@attakei.net>
2025-04-29 04:11:19 +0900

    ci: Add simple test

---
+++ _nim/tests/test_engine.nim
@@ -0,0 +1,10 @@
+import
+  std/unittest
+import
+  semver
+include
+  age/engine
+
+test "Initialized state of engine":
+  let engine = newEngine(newVersion(1,2,3), newVersion(1,2,4))
+  check(engine.rules.len == 0)
