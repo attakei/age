de37af2242ab9e7c9ba9b9cc48d4f03c2151a433
Kazuya Takei <myself@attakei.net>
2025-04-29 20:42:03 +0900

    feat: Set version info for cligen-config

--- _nim/src/age.nim
+++ _nim/src/age.nim
@@ -4,12 +4,6 @@ import std/strformat
 import cligen
 import ./age/[cli, consts]
 
-proc cli(version: bool = false, args: seq[string]): int =
-  result = 1
-  if version:
-    result = 0
-    echo(fmt"{NAME} v{VERSION}")
-    return
-
 when isMainModule:
+  clCfg.version = fmt"{NAME} v{VERSION}"
   dispatchMulti([info], [update], [major], [minor], [patch], [init], [help])
